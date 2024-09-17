bd20a985112d13253ef2f1ab8992b259c737cbb3
Jan Kordoš <jan.kordos@student.spsezatec.cz>
2024-09-17 11:53:58 +0200

    FIX

--- Build.lua
+++ Build.lua
@@ -1,8 +1,9 @@
--- premake5.lua
 workspace "BreakOutRaylibKordos"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
    startproject "App"
+
+   SolutionDir = path.getabsolute(".")
 
    -- Workspace-wide build options for MSVC
    filter "system:windows"
@@ -15,3 +16,51 @@ group "Core"
 group ""
 
 include "App/Build-App.lua"
+
+-- Global include directories for all projects if necessary
+IncludeDir = {}
+IncludeDir["Raylib"] = "$(SolutionDir)Vendor/Raylib/include"
+
+-- Global library directories if necessary
+LibDir = {}
+LibDir["Raylib"] = "$(SolutionDir)Vendor/Raylib/lib"
+
+project "App"
+   location "App"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir ("bin/" .. OutputDir .. "/%{prj.name}")
+   objdir ("bin-int/" .. OutputDir .. "/%{prj.name}")
+
+   files { "App/src/**.h", "App/src/**.cpp" }
+
+   -- Include directories for Raylib dependency
+   includedirs
+   {
+      "%{IncludeDir.Raylib}",  -- Include Raylib headers
+   }
+
+   -- Library directories for Raylib dependency
+   libdirs
+   {
+      "%{LibDir.Raylib}",  -- Link against Raylib library
+   }
+
+   -- Link with the Raylib library
+   links
+   {
+      "raylib", -- The actual library name, like raylib.lib on Windows
+      "winmm"
+   }
+
+   filter "system:windows"
+      cppdialect "C++17"
+      systemversion "latest"
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
